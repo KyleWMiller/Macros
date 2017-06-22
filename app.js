640fd2a7bb5563c1d297f7ed143848d47c924651
Kyle Miller <miller.kwill@gmail.com>
2017-06-22 15:18:19 -0500

    made headway #MDBW17

--- app.js
+++ app.js
@@ -1,44 +1,77 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('microApp', [])
-      .controller('microController', ['microFactory', microController])
-      .factory('microFactory', ['$http', microFactory])
+  angular.module('microApp', [])
+    .controller('microController', ['microFactory', microController])
+    .factory('microFactory', ['$http', microFactory])
 
-    function microController(microfactory) {
-      var mc = this,
-        mf = microfactory
+  function microController(microfactory) {
+    var mc = this,
+      mf = microfactory
 
-      mc.item = "food"
-      mc.list = [
-        {
-          bananas: {
-            protien: 2,
-            calories: 23,
-            fat: 17
-          }
-        },
-        {
-          coconutOil: {
+    mc.item = {}
+    mc.itemList = [{
+        name: "Banana",
+        protien: 2,
+        calories: 23,
+        fat: 17
+
+      },
+      {
+        name: "Coconut Oil",
+        protien: 3,
+        calories: 16,
+        fat: 11
+      }
+    ]
+
+    mc.addItem = function() {
+      mc.list.push(mc.item)
+      mc.item = {}
+    }
+
+    mc.receipe = {}
+    mc.receipeList = [{
+        name: "Muffin",
+        ingredients: [
+          { name: "Coconut Oil",
             protien: 3,
             calories: 16,
             fat: 11
+          },
+          { name: "Banana",
+              protien: 2,
+              calories: 23,
+              fat: 17
           }
-        }
-      ]
+        ]
+      }
 
+    ]
+
+    mc.addReceipeItem = function(item) {
+      mc.receipe.ingredient.push(item)
     }
 
+    mc.addReceipe = function() {
+      mc.receipeList.push(mc.receipe)
+      mc.receipe = {}
+    }
 
-      function microFactory($http) {
-        var mf = this,
-          getDbUrl = "api/items",
-          storeItemDbUrl = "api/storeItems"
+    // console.log(mc.listKeys)
 
-        mf.getDb = function(getDbUrl) {
-          return $http.get(getDbUrl)
-        }
+  }
 
-        return mf
-      }
-    }());
+
+  function microFactory($http) {
+    var mf = this,
+      getDbUrl = "api/items",
+      storeItemDbUrl = "api/storeItems"
+
+    mf.getDb = function(getDbUrl) {
+      return $http.get(getDbUrl)
+    }
+
+    return mf
+  }
+}());
